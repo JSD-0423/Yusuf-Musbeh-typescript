9f1364067dce55b7fba6e9f1aa1f98e4620945a3
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-06-06 13:07:36 +0300

    refactor: remove unnecessary error validation

--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -4,17 +4,7 @@ import {Book} from "../models/book";
 import * as validator from "express-validator";
 
 const getBooks = (request: Request, response: Response, next: NextFunction) => {
-    let query: any = "";
-    const errors = validator.validationResult(request);
-    if (!errors.isEmpty()) {
-        return response.status(422).json({
-            error: {
-                code: 422,
-                message: errors.array()[0].msg,
-            },
-        });
-    }
-    if (request.query.name) query = request.query.name;
+    let query: any = request.query.name || "";
     const books = getData()
     const filteredBooks: Book[] = books.filter((book: Book) => book.name.toLowerCase().startsWith(query));
     return response.status(200).json({statusCode: 200, books: filteredBooks});
