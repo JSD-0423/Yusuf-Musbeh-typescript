bee1969fbb0bee19a4fde0e2b48b5b9f2b8a9241
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-06-06 13:09:55 +0300

    refactor: improve error handling

--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -4,10 +4,16 @@ import {Book} from "../models/book";
 import * as validator from "express-validator";
 
 const getBooks = (request: Request, response: Response, next: NextFunction) => {
-    let query: any = request.query.name || "";
-    const books = getData()
-    const filteredBooks: Book[] = books.filter((book: Book) => book.name.toLowerCase().startsWith(query));
-    return response.status(200).json({statusCode: 200, books: filteredBooks});
+    try {
+        let query: any = request.query.name || "";
+        const books = getData()
+        const filteredBooks: Book[] = books.filter((book: Book) => book.name.toLowerCase().startsWith(query));
+        return response.status(200).json({statusCode: 200, books: filteredBooks});
+    } catch (e) {
+        console.error(e);
+        response.status(500).json({statusCode: 500, error: "Internal server error"})
+    }
+
 }
 
 export {getBooks}
