69369d93ca679ee01fd8d853ac48d39064bb786f
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-06-16 14:10:48 +0300

    feat: create endpoint to get all authors with thier books

--- controllers/author-controller.ts
+++ controllers/author-controller.ts
@@ -1,7 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { Author } from "../db/models/author";
+import { Book } from "../db/models/book";
 import * as crypto from "crypto";
 
+const getAuthors = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const authors = await Author.findAll({
+      include: [{ model: Book }],
+      attributes: ["name"],
+    });
+    response.status(200).json({ statusCode: 200, authors: authors });
+  } catch (e) {
+    console.error(e);
+    response
+      .status(500)
+      .json({ statusCode: 500, errors: ["Internal server error"] });
+  }
+};
 const postAuthors = async (
   request: Request,
   response: Response,
@@ -24,4 +43,4 @@ const postAuthors = async (
   }
 };
 
-export { postAuthors };
+export { postAuthors, getAuthors };
--- routes/author-route.ts
+++ routes/author-route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { postAuthors } from "../controllers/author-controller";
+import { getAuthors, postAuthors } from "../controllers/author-controller";
 import {
   handleErrors,
   postAuthorsValidationSchema,
 } from "../middlewares/validationMiddleware";
 import { checkSchema } from "express-validator";
 const router = Router();
-
+router.get("/authors", getAuthors);
 router.post(
   "/authors",
   checkSchema(postAuthorsValidationSchema),
